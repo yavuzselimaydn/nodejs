cd25fe6c1934bace4127d829c223bac2713fec30
yavuzselimaydn <yavuzselimaydn3@gmail.com>
2024-08-22 15:30:19 +0300

    routing altyapısı kuruldu #1

---
+++ api/db/models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(     //şema mongo dbde saklanacak belgelerin yapısını belirler.
+    {
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        is_active: { type: Boolean, default: true },
+        first_name: String,
+        last_name: String,
+        phone_number: String
+    },
+    {
+        //timestaps : true
+        timestamps: {
+            createdAt: "created_at",
+            updateAt: "updated_at"
+        }
+    }
+);
+
+class Users extends mongoose.Model {
+
+}
+
+schema.loadClass(Users);                             //user sınıfını semaya yukler, buda user sınıfındaki metotların mongoose modeline eklemeye yarar.
+module.exports = mongoose.model("users", schema);    //semaya dayalı mongoose modelini disa aktarır.
